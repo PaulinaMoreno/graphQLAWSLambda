e3f8212e4bab700597af45c5d9a792084503feef
PaulinaMoreno <elizabeth.moreno.aguilera@gmail.com>
2018-08-03 12:52:00 +0100

    Fix User resolver to retrieve friends

--- data.py
+++ data.py
@@ -1,81 +1,77 @@
-import boto3
-import os
-import json
+
 import sys
-from botocore.exceptions import ClientError
-from botocore.errorfactory import BaseClientExceptions
-from os.path import dirname, join
+import json
+import requests
+from requests_aws_sign import AWSV4Sign
+from boto3 import session
+sys.path.append(".") 
+user_data = []
+friendShip_data = []
+
+def setup():
+    
+    from schema import User, FriendShip
+    global user_data
+
+    user01 = User(
+        userID="1000",
+        name="Roxie Joddens",
+        email="rjoddens7@51.la",
+        phone="+358-119-977-5220",
+        friends=[],
+    
+    )
+
+    user02 = User(
+        userID="1001",
+        name="Aundrea Izard",
+        email="aizard8@biglobe.ne.jp",
+        phone="+4084687956",
+        friends=[],
+    )
+
+    user03 = User(
+        userID="1002",
+        name="Teddy Bolan",
+        email="tbologan@gmail.com",
+        phone="+4084689120",
+        friends=[],
+    )
+
+    user04 = User(
+        userID="1003",
+        name="Avril Joyson",
+        email="ajoysoni@tmall.com",
+        phone="+4086884546",
+        friends=[],
+    )
+
+    user01.friends.append(user02)
+    user01.friends.append(user04)
+
+    user02.friends.append(user04)
+    user02.friends.append(user03)
+
+    user03.friends.append(user02)
+    
+
+    user_data.append(user01)
+    user_data.append(user02)
+    user_data.append(user03)
+    user_data.append(user04)
 
 
-class DynamoDB:
-    def __init__(self):
-        self.connection = boto3.resource('dynamodb', region_name='us-west-2')
+def set_friendship_value(f):
+    print (f.userID)
+    friendShip_data.append(f)
+def delete_friendship_value(u):
+    for f in friendShip_data:
+        if f.userID == u.userID and f.friendID == u.friendID:
+            friendShip_data.remove(f)
+def get_friendship(friends):
+    for f in friends:
+        print(f.userID)
 
-    def buildExpression(self, data):
-        vals = {}
-        exp = 'SET '
-        attr_names = {}
-        for key, value in data.items():
-            vals[':{}'.format(key)] = value
-            attr_names['{}'.format(key)] = key
-            exp += '{} = :{},'.format(key, key)
-        exp = exp.rstrip(",")
-        return vals, exp
-
-    def put_item(self,Table, Item, ConditionExpression ):
-        try:
-            table = self.connection.Table(Table)
-            response_user_to_friend = table.put_item(
-                Item,
-                ConditionExpression
-            )
-            return response_user_to_friend
-        except ClientError as e:
-            raise e
-    def delete_Items(self, Table, l_items, keyID_pr, KeyID_sec):
-        try:
-            table = self.connection.Table(Table)
-            with table.batch_writer() as batch:
-                for item in l_items:
-                    print(item)
-                    batch.delete_item(
-                        Key={
-                                keyID_pr: item[keyID_pr],
-                                KeyID_sec: item[KeyID_sec]
-                            }
-                        )
-                    print(batch)
-            return True
-        except ClientError as e:
-            raise e
-
-    def update(self, table, keyID, keyValue, dataParamethers):
-        table = self.connection.Table(table)
-        vals, update_expression = self.buildExpression(dataParamethers)
-        try:
-            ExpressionAttributeValues = vals,
-            UpdateExpression = update_expression
-
-            result = table.update_item(
-                Key={
-                    keyID: keyValue
-                },
-                ExpressionAttributeValues=vals,
-                UpdateExpression=update_expression,
-                ReturnValues='ALL_NEW',
-        )
-            responseJson = {
-                "statusCode": 200,
-                "headers": {"Content-Type": 'application/json'},
-                "body": 'Update user on dynamodb'
-            }
-            return responseJson
-        except Exception as err:
-            responseJson = {
-                "statusCode": 400,
-                "headers": {"Content-Type": 'application/json'},
-                "body": str(err)
-            }
-            print(err)
-            return responseJson
+def get_all_users():
+    return user_data
 
